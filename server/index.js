6a589ee265997181ecd77d2456b6316a7ee48181
marko-z <m.zakrzwsk@gmail.com>
2021-07-23 14:04:46 +0200

    Added basic io functionality

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 
+
 const app = express();
 const server = http.createServer(app);
 
@@ -9,16 +10,20 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.use(express.static(__dirname + '/public/'))
+
+io.on('connection', (socket) => {
+	console.log('A user connected!');
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+});
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
 
-app.get('/', (req, res) => {
-	res.send('hello world');
-});
-
-app.listen(3000, () => {
+server.listen(3000, () => {
 	console.log('Listening on 3000');
 });
 
